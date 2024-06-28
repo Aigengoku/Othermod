34d88c706778438cc7e29a0df7f113902eeb2ec2
Aigengoku <youcef5908@gmail.com>
2024-06-28 11:53:01 -0400

    Add files via upload

---
+++ Stupidity.lua
@@ -0,0 +1,18 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Stupidity
+--- MOD_ID: Stupidity
+--- MOD_AUTHOR: [Ai gen goku]
+--- MOD_DESCRIPTION: Stupidity awaits !```
+--- MOD_PREFIX: stupidity
+----------------------------------------------
+------------MOD CODE -------------------------
+
+-- made so that other mods in the stupidity genre works
+
+NFS.load(SMODS.current_mod.path.."Stupidities S/Stupidity_Jokers.lua")()
+NFS.load(SMODS.current_mod.path.."Stupidities S/Stupidity_Tarots.lua")()
+NFS.load(SMODS.current_mod.path.."Stupidities S/Stupidity_Vouchers.lua")()
+NFS.load(SMODS.current_mod.path.."Stupidities S/stupidity_Enhancements.lua")()
+
+----------------------------------------------
+------------MOD CODE END----------------------
